ccf908b33a9852b6d75e4f01ec589c9c5001a551
dpalomar <dpalomar@gmail.com>
2016-12-21 01:41:37 +0100

    initial

---
+++ src/main/webapp/app/entities/bank-account/bank-account.controller.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('jhipsterSampleApplicationApp')
+        .controller('BankAccountController', BankAccountController);
+
+    BankAccountController.$inject = ['$scope', '$state', 'BankAccount'];
+
+    function BankAccountController ($scope, $state, BankAccount) {
+        var vm = this;
+
+        vm.bankAccounts = [];
+
+        loadAll();
+
+        function loadAll() {
+            BankAccount.query(function(result) {
+                vm.bankAccounts = result;
+                vm.searchQuery = null;
+            });
+        }
+    }
+})();
